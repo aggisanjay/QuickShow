b9d5749fcba279a259439d2f4d5ed2e1a49de278
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-06-27 19:40:17 +0530

    function added to release after 10 min

--- server/controller/bookingController.js
+++ server/controller/bookingController.js
@@ -2,6 +2,7 @@
 
 //function to check availability of selected seats for a movie
 
+import { inngest } from "../inngest/index.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js"
 import stripe from "stripe"
@@ -78,6 +79,12 @@ export const createBooking=async(req,res)=>{
        })
        booking.paymentLink=session.url;
        await booking.save();
+       //run inngest shedular function to check payment status after 10 minutes
+
+       await inngest.send({
+        name:'app/checkpayment',
+        data:{bookingId:booking._id.toString()}
+       })
        res.json({success:true,url:session.url})
 
 
